130be415b9b9004fe04661ec0f806f3365b4ee8c
Maria <maria.rudolphi@hotmail.com>
2018-09-11 12:20:19 +0200

    Complete webpage - version 1

---
+++ src/js/images.js
@@ -0,0 +1,5 @@
+"use strict";
+
+function images() {
+	document.getElementById("images").innerHTML = "<h3>Det här är några random bilder från <a href='https://www.flickr.com/' target='_blank'>Flickr</a>.</h3>";
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,17 @@
 
 var today = new Date();
 var year = today.getFullYear();
-var month = today.getMonth() + 1;
+var month = today.getMonth() +1;
 var day = today.getDate();
-document.getElementById("start").innerHTML += "Idag är det " + year + ":" + month + ":" + day;
+
+function start() {
+	if (month < 10) {
+		month = "0" + month;
+	}
+
+	if (day < 10) {
+		day = "0" + day;
+	}
+
+	document.getElementById("start").innerHTML = "<h3>Idag är det " + year + "-" + month + "-" + day + "</h3>";
+}
